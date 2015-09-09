382cabd134d5bc1e2ffc39e27e4f7658b5860a71
aarontholt <holtat@colorado.edu>
2015-09-08 18:18:38 -0600

    Update Ring.c to handle 1 process

--- Ring.c
+++ Ring.c
@@ -20,29 +20,35 @@ int main(int argc, char** argv) {
     MPI_Get_processor_name(processor_name, &name_len);
 
 
-    //Send something in messages, not really needed in this example
+    //Send something in messages to prove they were sent from a specific process
     int number;
 
     //Process 0
     if (world_rank == 0)
     {
+        //Initilize number to be the process number
         number = 0;
-        //Send to process 1, recv from process world_size
-        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Recv(&number, 1, MPI_INT, world_size-1, 0, MPI_COMM_WORLD, 
-            MPI_STATUS_IGNORE);
-        printf("Process %d received from process %d \n", world_rank, world_size-1);
+        //Send to process 1 if world size>1
+        //Send to self if world size is 1
+        if (world_size > 1)
+        {
+            MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD); 
+            //Recv from last process.
+            MPI_Recv(&number, 1, MPI_INT, world_size-1, 0, MPI_COMM_WORLD, 
+                MPI_STATUS_IGNORE);  
+        }
+        printf("Process %d received from process %d \n", world_rank, number);
     }
     else if (world_rank != 0)
     {
         //Receive from process rank-1
         MPI_Recv(&number, 1, MPI_INT, world_rank-1, 0, MPI_COMM_WORLD, 
             MPI_STATUS_IGNORE);
-        printf("Process %d received from process %d \n", world_rank, world_rank-1);
-
+        printf("Process %d received from process %d \n", world_rank, number);
 
         //If its the last processor send to process 0,
         //Otherwise send to rank+1
+        number = world_rank;
         if (world_rank == world_size-1)
         {
             MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
@@ -51,10 +57,7 @@ int main(int argc, char** argv) {
         {
             MPI_Send(&number, 1, MPI_INT, world_rank+1, 0, MPI_COMM_WORLD);
         }
-        
-
     }
-
 
     // Finalize the MPI environment.
     MPI_Finalize();

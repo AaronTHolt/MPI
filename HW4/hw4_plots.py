a7648059a85aa8dd84c21690ee7b657a5dce4811
aarontholt <holtat@colorado.edu>
2015-09-29 18:03:36 -0600

    first plot

--- HW4/hw4_plots.py
+++ HW4/hw4_plots.py
@@ -1,8 +1,16 @@
 import numpy as np
-# import scipy as sp
-# import scipy.stats
+import scipy as sp
+import scipy.stats
 import matplotlib.pyplot as plt
 
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0*np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+    # return [m, [m-h, m+h]]
+    return [m, h]
 
 serial_576 = []
 serial_576_stats = []
@@ -12,19 +20,23 @@ fox_4_576 = []
 fox_4_576_stats = []
 
 for ii,name in enumerate(['all_4_576','fox_4_576','serial_576']):
-	with open('RESULTS/{ff}'.format(ff=ii)) as f:
+	with open('RESULTS/{ff}'.format(ff=name)) as f:
 		# print "RESULTS/results_pp_sn_{s}".format(s=size[ii])
-		lis = [line.split() for line in f]
-		print lis
-		mylist = lis[0][0].split(",")
+		firstline = f.readline()
+		lis = firstline.split()
+		mylist = lis[0].split(",")
 		mylist2 = [float(x)/2 for x in mylist]
 		if ii==0:
 			all_4_576.append(mylist2)
-			print mylist2
+			all_4_576_stats=mean_confidence_interval(mylist2)
+			# print mylist2
 		if ii==1:
 			fox_4_576.append(mylist2)
+			fox_4_576_stats=mean_confidence_interval(mylist2)
 		if ii==2:
-			serial_576.append(mylist2)	
+			serial_576.append(mylist2)
+			serial_576_stats=mean_confidence_interval(mylist2)
+			# print serial_576_stats
 	f.close()
 
 
@@ -34,47 +46,36 @@ for ii,name in enumerate(['all_4_576','fox_4_576','serial_576']):
 # for ii in [4,16,36,64,144]:
 # 
 
-# def mean_confidence_interval(data, confidence=0.95):
-#     a = 1.0*np.array(data)
-#     n = len(a)
-#     m, se = np.mean(a), scipy.stats.sem(a)
-#     h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
-#     # return [m, [m-h, m+h]]
-#     return [m, h]
-
-# for ii in range(0,len(pp_sn)):
-# 	pp_sn_stats.append(mean_confidence_interval(pp_sn[ii]))
-# 	pp_ln_stats.append(mean_confidence_interval(pp_ln[ii]))
-# 	pp_rn_stats.append(mean_confidence_interval(pp_rn[ii]))
 
 
 
-# #plot (x,y,style)
-# line0, = plt.plot(size, [row[0] for row in pp_sn_stats], 'ro-')
-# plt.errorbar(size, [row[0] for row in pp_sn_stats], 
-# 	yerr=[row[1] for row in pp_sn_stats], fmt='ro-')
+#plot (x,y,style)
+line0, = plt.plot(1, serial_576_stats[0], 'ro-')
+plt.errorbar(1, serial_576_stats[0], 
+	yerr=serial_576_stats[1], fmt='ro-')
 
-# line1, = plt.plot(size, [row[0] for row in pp_ln_stats], 'bs-')
-# plt.errorbar(size, [row[0] for row in pp_ln_stats], 
-# 	yerr=[row[1] for row in pp_ln_stats], fmt='bs-')
+line1, = plt.plot(4, fox_4_576_stats[0], 'bs-')
+plt.errorbar(4, fox_4_576_stats[0], 
+	yerr=fox_4_576_stats[1], fmt='bs-')
 
-# line2, = plt.plot(size, [row[0] for row in pp_rn_stats], 'k^-')
-# plt.errorbar(size, [row[0] for row in pp_rn_stats], 
-# 	yerr=[row[1] for row in pp_rn_stats], fmt='k^-')
+line2, = plt.plot(4, all_4_576_stats[0], 'kx-')
+plt.errorbar(4, all_4_576_stats[0], 
+	yerr=all_4_576_stats[1], fmt='kx-')
 
-# #title and axis labels
-# plt.xlabel('Size of buffer (Bytes)')
-# plt.ylabel('Time (s)')
-# plt.title("Time vs Buffer Size for Exponential Message Size Growth")
-# #log2 scale
+#title and axis labels
+plt.xlabel('Number of Nodes')
+plt.ylabel('Time (s)')
+plt.title("Time vs Number of Nodes Serial vs Parallel Algorithms")
+#log2 scale
 # plt.xscale('log', basex=2)
 # plt.yscale('log', basey=2)
-# #x axis limits (set just above last value)
-# plt.xlim([0,2**23+2**22])
-# #legend
-# plt.legend([line0,line1,line2], 
-# 	['SingleNode', 'LeafNodes', 'RandomNodes'], loc=2)
-# plt.show()
+#x axis limits (set just above last value)
+plt.xlim([0,5])
+plt.ylim([0,1.2])
+#legend
+plt.legend([line0,line1,line2], 
+	['Serial', 'Fox', 'Allgather'], loc=1)
+plt.show()
 
 # ################################################################
 

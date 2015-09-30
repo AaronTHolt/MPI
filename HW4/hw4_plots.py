f04af24cfe270bc5346ba9fe735ba7b3eeca5a64
aarontholt <holtat@colorado.edu>
2015-09-29 23:18:22 -0600

    asdf

--- HW4/hw4_plots.py
+++ HW4/hw4_plots.py
@@ -39,13 +39,74 @@ for ii,name in enumerate(['all_4_576','fox_4_576','serial_576']):
 			# print serial_576_stats
 	f.close()
 
+all_np = []
+all_np_stats = []
+fox_np = []
+fox_np_stats = []
+
+p = [4,16,36,64,144]
+for ii in [4,16,36,64,144]:
+	with open('RESULTS/all_{np}_144'.format(np=ii)) as f:
+		firstline = f.readline()
+		lis = firstline.split()
+		mylist = lis[0].split(",")
+		mylist2 = [float(x)/2 for x in mylist]
+		all_np.append(mylist2)
+		all_np_stats.append(mean_confidence_interval(mylist2))
+	with open('RESULTS/fox_{np}_144'.format(np=ii)) as f:
+		firstline = f.readline()
+		lis = firstline.split()
+		mylist = lis[0].split(",")
+		mylist2 = [float(x)/2 for x in mylist]
+		fox_np.append(mylist2)
+		fox_np_stats.append(mean_confidence_interval(mylist2))
+		# print fox_np_stats
+
+all_size = []
+all_size_stats = []
+fox_size = []
+fox_size_stats = []
+
+# size = [144,576,1296,2304]
+# size = [144, 576]
+# for ii in [144,576]:
+# 	with open('RESULTS/all_144_{ss}'.format(ss=ii)) as f:
+# 		firstline = f.readline()
+# 		lis = firstline.split()
+# 		mylist = lis[0].split(",")
+# 		mylist2 = [float(x)/2 for x in mylist]
+# 		all_size.append(mylist2)
+# 		all_size_stats.append(mean_confidence_interval(mylist2))
+# 	with open('RESULTS/fox_144_{ss}'.format(ss=ii)) as f:
+# 		firstline = f.readline()
+# 		lis = firstline.split()
+# 		mylist = lis[0].split(",")
+# 		mylist2 = [float(x)/2 for x in mylist]
+# 		fox_size.append(mylist2)
+# 		fox_size_stats.append(mean_confidence_interval(mylist2))
 
 
-# for ii in [144,576,1296,2304]:
-# 	pass
-# for ii in [4,16,36,64,144]:
-# 
+# line0, = plt.plot(size, [row[0] for row in all_size_stats], 'ro-')
+# plt.errorbar(size, [row[0] for row in all_size_stats], 
+# 	yerr=[row[1] for row in all_size_stats], fmt='ro-')
 
+# line1, = plt.plot(size, [row[0] for row in fox_size_stats], 'bs-')
+# plt.errorbar(size, [row[0] for row in fox_size_stats], 
+# 	yerr=[row[1] for row in fox_size_stats], fmt='bs-')
+
+# #title and axis labels
+# plt.xlabel('Size of Matrix (doubles)')
+# plt.ylabel('Time (s)')
+# plt.title("Size of Matrix vs Time for NP=144")
+# plt.xscale('log', basex=2)
+# plt.yscale('log', basey=2)
+# # plt.ylim([2**-13,2**-5])
+# # plt.ylim([0,7.5])
+# # plt.xticks([0,512,1024,1536,2048,2560,3072,3584,4096])
+# #legend
+# plt.legend([line0,line1], 
+# 	['Allgather', 'Fox'], loc=2)
+# plt.show()
 
 
 
@@ -65,7 +126,7 @@ plt.errorbar(4, all_4_576_stats[0],
 #title and axis labels
 plt.xlabel('Number of Nodes')
 plt.ylabel('Time (s)')
-plt.title("Time vs Number of Nodes Serial vs Parallel Algorithms")
+plt.title("Time vs Number of Nodes for a 576X576 Matrix of Doubles")
 #log2 scale
 # plt.xscale('log', basex=2)
 # plt.yscale('log', basey=2)
@@ -77,29 +138,26 @@ plt.legend([line0,line1,line2],
 	['Serial', 'Fox', 'Allgather'], loc=1)
 plt.show()
 
-# ################################################################
+################################################################
 
-# for ii in range (0,len(small)):
-# 	pp_ln_small_stats.append(mean_confidence_interval(pp_ln_small[ii]))
-# 	pp_rn_small_stats.append(mean_confidence_interval(pp_rn_small[ii]))
 
-# line0, = plt.plot(small, [row[0]*(10**6) for row in pp_rn_small_stats], 'ro-')
-# plt.errorbar(small, [row[0]*(10**6) for row in pp_rn_small_stats], 
-# 	yerr=[row[1]*(10**6) for row in pp_rn_small_stats], fmt='ro-')
+line0, = plt.plot(p, [row[0] for row in all_np_stats], 'ro-')
+plt.errorbar(p, [row[0] for row in all_np_stats], 
+	yerr=[row[1] for row in all_np_stats], fmt='ro-')
 
-# line1, = plt.plot(small, [row[0]*(10**6) for row in pp_ln_small_stats], 'bs-')
-# plt.errorbar(small, [row[0]*(10**6) for row in pp_ln_small_stats], 
-# 	yerr=[row[1]*(10**6) for row in pp_ln_small_stats], fmt='bs-')
+line1, = plt.plot(p, [row[0] for row in fox_np_stats], 'bs-')
+plt.errorbar(p, [row[0] for row in fox_np_stats], 
+	yerr=[row[1] for row in fox_np_stats], fmt='bs-')
 
-# #title and axis labels
-# plt.xlabel('Size of buffer (Bytes)')
-# plt.ylabel('Time (Microseconds)')
-# plt.title("Time vs Buffer Size for Latency Level Buffer Sizes")
-# plt.xlim([0,4300])
-# plt.ylim([0,7.5])
-# plt.xticks([0,512,1024,1536,2048,2560,3072,3584,4096])
-# #legend
-# plt.legend([line1,line0], 
-# 	['LeafNodes', 'RandomNodes'], loc=2)
-# plt.show()
+#title and axis labels
+plt.xlabel('Processors')
+plt.ylabel('Time (s)')
+plt.title('Number of Processors vs Time for 144x144')
+plt.xscale('log', basex=2)
+plt.yscale('log', basey=2)
+plt.ylim([2**-13,2**-5])
+#legend
+plt.legend([line0,line1], 
+	['Allgather', 'Fox'], loc=3)
+plt.show()
 
